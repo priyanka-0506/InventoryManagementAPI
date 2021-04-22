635f6d4882458d6948e00b6770cf8c470fd27ba2
Priyanka Malviya <32741340+priyanka-0506@users.noreply.github.com>
2021-04-22 15:54:20 +0530

    changes related to setup database in the api

---
+++ InventoryManagementAPI/Controllers/InventoryManagementController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryManagementAPI.Controllers
+{
+    public class InventoryManagementController : Controller
+    {
+        // GET: InventoryManagementController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: InventoryManagementController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: InventoryManagementController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: InventoryManagementController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: InventoryManagementController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: InventoryManagementController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: InventoryManagementController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: InventoryManagementController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
